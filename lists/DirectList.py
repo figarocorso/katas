aa9a2820ceb9387daef57592b69da4796be47ce0
Miguel Julián <mjulian@zentyal.com>
2013-01-16 02:22:29 +0100

    Lists kata: first approach to "one way" lists

---
+++ lists/DirectList.py
@@ -0,0 +1,40 @@
+class DirectNode():
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next_node = None
+
+    def __str__(self):
+        return '(' + str(self.key) + ',' + str(self.value) + ',' + str(self.next_node) + ')'
+
+class DirectList():
+
+    def __init__(self):
+        self.first_node = DirectNode(None,None)
+        self.end_of_list = DirectNode(None,None)
+        self.last_node = self.first_node
+
+    def add(self, node):
+        self.last_node.next_node = node
+        node.next_node = self.end_of_list
+        self.last_node = node
+
+        return node.value
+
+    def find(self, key):
+        self.current_node = self.first_node
+
+        while not self.is_last_node(self.current_node):
+            if self.current_node.key == key:
+                return self.current_node.value
+
+            self.current_node = self.current_node.next_node
+
+        return None
+
+    def is_last_node(self, node):
+        return node.next_node == None
+
+    def __str__(self):
+        return str(self.first_node)
---
+++ lists/DirectListTest.py
@@ -0,0 +1,41 @@
+import unittest
+from DirectList import *
+
+class DirectListTest(unittest.TestCase):
+
+    def setUp(self):
+        self.list1 = DirectList()
+
+    def testAddingAElement(self):
+        self.nodePerra = DirectNode('perrakey','perra')
+        self.assertEqual("perra", self.list1.add(self.nodePerra))
+
+    def testProperAdding(self):
+        self.assertEqual(None, self.list1.find('perrakey'))
+        self.nodePerra = DirectNode('perrakey','perra')
+        self.assertEqual(None, self.list1.find('perrakey'))
+
+    def testFindingAElement(self):
+        self.nodePerra = DirectNode('perrakey','perra')
+        self.list1.add(self.nodePerra)
+        self.assertEqual("perra", self.list1.find('perrakey'))
+
+    def testFindingAElementFromMultipleNodeList(self):
+        self.nodePerra = DirectNode('perrakey','perra')
+        self.list1.add(self.nodePerra)
+        self.node_vaca = DirectNode('vacakey','vaca')
+        self.list1.add(self.node_vaca)
+        self.assertEqual("perra", self.list1.find('perrakey'))
+
+    def testFindingWrongKey(self):
+        self.nodePerra = DirectNode('perrakey','perra')
+        self.list1.add(self.nodePerra)
+        self.node_vaca = DirectNode('vacakey','vaca')
+        self.list1.add(self.node_vaca)
+        self.assertEqual(None, self.list1.find('burrakey'))
+
+    def tearDown(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
