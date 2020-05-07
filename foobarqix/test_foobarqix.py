22097ff0f777430d25cc2a9d5b39b5fd06fc692d
figarocorso <figarocorso@gmail.com>
2020-05-07 09:41:08 +0200

    Improve FooBarQix test naming

--- foobarqix/test_foobarqix.py
+++ foobarqix/test_foobarqix.py
@@ -11,35 +11,37 @@ class TestFooBarQix(unittest.TestCase):
     def test_1(self):
         self.assertEqual("1", self.foobarqix.compute("1"))
 
-
-    def test_2(self):
+    def test_no_compute(self):
         self.assertEqual("2", self.foobarqix.compute("2"))
 
-    def test_6(self):
+    def test_divisible_by_3(self):
         self.assertEqual("Foo", self.foobarqix.compute("6"))
 
-    def test_3(self):
+    def test_contains_a_3(self):
+        self.assertEqual("Foo", self.foobarqix.compute("13"))
+
+    def test_contains_a_3_and_divisible_by_3(self):
         self.assertEqual("FooFoo", self.foobarqix.compute("3"))
 
-    def test_20(self):
+    def test_divisible_by_5(self):
         self.assertEqual("Bar", self.foobarqix.compute("20"))
 
-    def test_5(self):
+    def test_contains_a5_and_divisible_by_5(self):
         self.assertEqual("BarBar", self.foobarqix.compute("5"))
 
-    def test_45(self):
+    def test_divisible_by_3_and_5_contains_a_3(self):
         self.assertEqual("FooBarBar", self.foobarqix.compute("45"))
 
-    def test_7(self):
+    def test_divisible_by_3_and_contains_a_7(self):
         self.assertEqual("QixQix", self.foobarqix.compute("7"))
 
-    def test_75(self):
+    def test_contains_5_and_7_unordered(self):
         self.assertEqual("FooBarQixBar", self.foobarqix.compute("75"))
 
-    def test_777(self):
+    def test_contains_multiple_7(self):
         self.assertEqual("FooQixQixQixQix", self.foobarqix.compute("777"))
 
-    def test_753(self):
+    def test_contains_3_and_5_and_7_unordered(self):
         self.assertEqual("FooQixBarFoo", self.foobarqix.compute("753"))
 
 if __name__ == "__main__":
