4d6b16fb93a6279a1a07ed4b7cfc7bbf5dad292e
figarocorso <figarocorso@gmail.com>
2020-12-20 23:49:50 +0100

    Improve Advent of Code 2020 3rd day

--- AoC-2020-03/aoc_2020_03.py
+++ AoC-2020-03/aoc_2020_03.py
@@ -1,8 +1,19 @@
 import unittest
 
 
+class WidthExceededException(Exception):
+    pass
+
+
+class DownValueNeededException(Exception):
+    pass
+
+
 class Slope():
     def __init__(self, right, down):
+        if down <= 0:
+            raise DownValueNeededException
+
         self.right = right
         self.down = down
 
@@ -12,6 +23,7 @@ class Topology():
 
     def __init__(self, topology_list):
         self.width = len(topology_list[0])
+        self.heigh = len(topology_list)
         self.topology = ''.join(topology_list)
         self.restart()
 
@@ -19,7 +31,13 @@ class Topology():
         return self.topology
 
     def restart(self):
-        self.current_position = 0
+        self.current_x = 0
+        self.current_y = 0
+
+    @property
+    def current_position(self):
+        return self.current_x + self.current_y * self.width
+
 
     def count_trees_during_slope_down(self, slope):
         trees = 0
@@ -29,10 +47,10 @@ class Topology():
         return trees
 
     def slope_down(self, slope):
-        self.advance(slope.right + self.width * slope.down)
-
-    def advance(self, positions):
-        self.current_position += positions
+        self.current_x += slope.right
+        self.current_y += slope.down
+        if self.current_x >= self.width and self.current_y < self.heigh:
+            raise WidthExceededException
 
     @property
     def is_finished(self):
@@ -65,22 +83,6 @@ class TopologySlopeTest(unittest.TestCase):
         self.assertEqual(726, len(str(self.topology)))
         self.assertEqual(66, self.topology.width)
 
-    def test_current_position(self):
-        self.assertEqual(0, self.topology.current_position)
-
-    def test_advance(self):
-        self.assertEqual(0, self.topology.current_position)
-        self.topology.advance(10)
-        self.assertEqual(10, self.topology.current_position)
-        self.topology.advance(len(str(self.topology)))
-        self.assertEqual(736, self.topology.current_position)
-
-    def test_slope_down_only_right(self):
-        slope = Slope(1, 0)
-        self.assertEqual(0, self.topology.current_position)
-        self.topology.slope_down(slope)
-        self.assertEqual(1, self.topology.current_position)
-
     def test_slope_down_only_down(self):
         slope = Slope(0, 1)
         self.assertEqual(0, self.topology.current_position)
@@ -97,35 +99,34 @@ class TopologySlopeTest(unittest.TestCase):
         self.topology.slope_down(slope)
         self.assertEqual(slope.right * 2 + self.topology.width * 2, self.topology.current_position)
 
+    def test_slope_down_over_right(self):
+        slope = Slope(self.topology.width, 1)
+        self.assertRaises(WidthExceededException, self.topology.slope_down, slope)
+
+    def test_slope_down_only_right(self):
+        self.assertRaises(DownValueNeededException, Slope, 1, 0)
+
     def test_is_finished(self):
         self.assertFalse(self.topology.is_finished)
-        self.topology.advance(10)
+        self.topology.slope_down(Slope(3, 1))
         self.assertFalse(self.topology.is_finished)
-        self.topology.advance(len(str(self.topology)))
-        self.assertTrue(self.topology.is_finished)
-
-    def test_is_finished_limits(self):
-        self.assertFalse(self.topology.is_finished)
-        self.topology.advance(len(str(self.topology)) - 1)
-        self.assertFalse(self.topology.is_finished)
-        self.topology.advance(1)
+        self.topology.slope_down(Slope(3, self.topology.heigh + 3))
         self.assertTrue(self.topology.is_finished)
 
     def test_type_of_square_tree(self):
+        slope = Slope(0, 1)
         self.assertFalse(self.topology.is_tree)
-        self.topology.advance(1)
+        self.topology.slope_down(slope)
+        self.assertTrue(self.topology.is_tree)
+        self.topology.slope_down(slope)
         self.assertFalse(self.topology.is_tree)
-        self.topology.advance(1)
-        self.assertTrue(self.topology.is_tree)
-        self.topology.advance(1)
-        self.assertTrue(self.topology.is_tree)
-        self.topology.advance(1)
+        self.topology.slope_down(slope)
         self.assertFalse(self.topology.is_tree)
 
     def test_restart(self):
         self.assertEqual(0, self.topology.current_position)
-        self.topology.advance(1)
-        self.assertEqual(1, self.topology.current_position)
+        self.topology.slope_down(Slope(0, 1))
+        self.assertEqual(self.topology.width, self.topology.current_position)
         self.topology.restart()
         self.assertEqual(0, self.topology.current_position)
 
@@ -133,9 +134,10 @@ class TopologySlopeTest(unittest.TestCase):
         self.assertEqual(7, self.topology.count_trees_during_slope_down(Slope(3, 1)))
         self.topology.restart()
         self.assertEqual(3, self.topology.count_trees_during_slope_down(Slope(0, 1)))
-        self.topology.restart()
-        self.assertEqual(str(self.topology).count(self.topology.TREE_CHAR),
-                         self.topology.count_trees_during_slope_down(Slope(1, 0)))
+
+    def test_count_trees_not_finished(self):
+        slope = Slope(30, 1)
+        self.assertRaises(WidthExceededException, self.topology.count_trees_during_slope_down, slope)
 
 
 if __name__ == '__main__':
